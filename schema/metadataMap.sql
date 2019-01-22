46b2a0d8c41ea4a1654229ff720681b4e91e2586
BenB196 <benbrenek@yahoo.com>
2019-01-21 20:02:01 -0500

    Added missing fields from metadataMap schema
    Added the missing metadataNameId and metadataValueId fields that are need to link metadata to collections/items.

--- schema/metadataMap.sql
+++ schema/metadataMap.sql
@@ -6,5 +6,7 @@ CREATE TABLE metadataMap(
   collectionItemId INT(4) NOT NULL, #Max value 2.14 billion (32-bit int), Required, This value will either be a collectionId or an itemId.
   collectionItemIdType INT(4) NOT NULL, #Max value 2.14 billion (32-bit int), Required, This value will be gotten from an enum, and will specify whether collectionItemId is either a collectionId or an itemId.
   metadataNameInheritance TINYINT(1) NOT NULL, #Same thing as Boolean 0/1, specifies whether the linking metadataName will be inherited by all sub collections/items, 1 means YES inherit/0 means no don't inherit.
-  metadataValueInheritance TINYINT(1) NOT NULL #Same thing as Boolean 0/1, specifies whether the linking metadataValue will be inherited by all sub collections/items, 1 mean YES inherit/0 means no don't inherit.
+  metadataNameId INT(4) NOT NULL, #Max value 2.14 billion (32-bit int), Required.
+  metadataValueInheritance TINYINT(1) NOT NULL, #Same thing as Boolean 0/1, specifies whether the linking metadataValue will be inherited by all sub collections/items, 1 mean YES inherit/0 means no don't inherit.
+  metadataValueId INT(4) NULL #Max value 2.14 billion (32-bit int), Required only if the map is linking directly to the item, or if metadataValueInheritance is equal to 1.
 );
