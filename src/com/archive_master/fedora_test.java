3df31ce9eb0fff12823a69581c3d81e92e13b544
BenB196 <benbrenek@yahoo.com>
2019-01-27 17:03:04 -0500

    Got Fedora API to work

--- src/com/archive_master/fedora_test.java
+++ src/com/archive_master/fedora_test.java
@@ -8,12 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.*;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @WebServlet("/test")
 public class fedora_test extends HttpServlet {
@@ -22,25 +18,28 @@ public class fedora_test extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String button = request.getParameter("button");
 
-		String url = "localhost:8080/rest/";
+
 
 		if ("button1".equals(button)) {
+			System.out.println("Creating URL");
+			URL url = new URL("http://localhost:8080/rest/");
 			System.out.println("Creating HTTP Client");
-			HttpClient client = HttpClientBuilder.create().build();
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			System.out.println("Creating GET Request");
-			HttpGet httpRequest = new HttpGet(url);
-			System.out.println("Executing/Getting Response");
-			HttpResponse httpResponse = client.execute(httpRequest);
+			con.setRequestMethod("GET");
 
-			System.out.println("Response Code: " + httpResponse.getStatusLine().getStatusCode());
+			System.out.println("Response Code: " + con.getResponseCode());
 
-			BufferedReader rd = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+			BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
 			StringBuffer result = new StringBuffer();
-			String line ="";
+			String line;
 			while ((line = rd.readLine()) != null) {
 				result.append(line);
 			}
+			System.out.println(result);
+			rd.close();
+			con.disconnect();
 		}
 	}
 }
