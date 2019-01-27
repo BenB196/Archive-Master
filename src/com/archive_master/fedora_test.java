25f5f04d6fd378caa7cd6565843519bdf1fd15d3
BenB196 <benbrenek@yahoo.com>
2019-01-27 16:30:47 -0500

    Added dependencies to maven pom.xml

--- src/com/archive_master/fedora_test.java
+++ src/com/archive_master/fedora_test.java
@@ -5,8 +5,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
-import org.apache.commons.httpclient.*;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 
 @WebServlet("/test")
@@ -16,8 +22,25 @@ public class fedora_test extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String button = request.getParameter("button");
 
+		String url = "localhost:8080/rest/";
+
 		if ("button1".equals(button)) {
-			System.out.println("Test");
+			System.out.println("Creating HTTP Client");
+			HttpClient client = HttpClientBuilder.create().build();
+			System.out.println("Creating GET Request");
+			HttpGet httpRequest = new HttpGet(url);
+			System.out.println("Executing/Getting Response");
+			HttpResponse httpResponse = client.execute(httpRequest);
+
+			System.out.println("Response Code: " + httpResponse.getStatusLine().getStatusCode());
+
+			BufferedReader rd = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+
+			StringBuffer result = new StringBuffer();
+			String line ="";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
 		}
 	}
 }
